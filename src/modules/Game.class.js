e24b88f9139ecdf6026d88ef6935415c2107901d
Vhatever <fserrorka@gmail.com>
2025-08-07 16:27:37 +0300

    2048

--- src/modules/Game.class.js
+++ src/modules/Game.class.js
@@ -1,44 +1,58 @@
 'use strict';
 
-/**
- * This class represents the game.
- * Now it has a basic structure, that is needed for testing.
- * Feel free to add more props and methods if needed.
- */
 class Game {
-  /**
-   * Creates a new game instance.
-   *
-   * @param {number[][]} initialState
-   * The initial state of the board.
-   * @default
-   * [[0, 0, 0, 0],
-   *  [0, 0, 0, 0],
-   *  [0, 0, 0, 0],
-   *  [0, 0, 0, 0]]
-   *
-   * If passed, the board will be initialized with the provided
-   * initial state.
-   */
-  constructor(initialState) {
-    // eslint-disable-next-line no-console
-    console.log(initialState);
+  constructor(
+    initialState = [
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+    ],
+  ) {
+    this.initialBoard = initialState;
+    this.board = JSON.parse(JSON.stringify(initialState));
+    this.boardScore = 0;
+    this.status = 'idle';
+    this.isGameWon = false;
+    this.isGameLose = false;
+    this.isAbleMove = true;
+    this.isGameActive = false;
   }
 
-  moveLeft() {}
-  moveRight() {}
-  moveUp() {}
-  moveDown() {}
+  moveLeft() {
+    if (this.isGameActive) {
+      this.move('left');
+    }
+  }
+  moveRight() {
+    if (this.isGameActive) {
+      this.move('right');
+    }
+  }
+  moveUp() {
+    if (this.isGameActive) {
+      this.move('up');
+    }
+  }
+  moveDown() {
+    if (this.isGameActive) {
+      this.move('down');
+    }
+  }
 
   /**
    * @returns {number}
    */
-  getScore() {}
+  getScore() {
+    return this.boardScore;
+  }
 
   /**
    * @returns {number[][]}
    */
-  getState() {}
+  getState() {
+    return this.board;
+  }
 
   /**
    * Returns the current game status.
@@ -50,19 +64,189 @@ class Game {
    * `win` - the game is won;
    * `lose` - the game is lost
    */
-  getStatus() {}
+  getStatus() {
+    switch (true) {
+      case this.isGameWon:
+        this.status = 'win';
+        break;
+      case this.isGameLose:
+        this.status = 'lose';
+        break;
+      case this.isGameActive:
+        this.status = 'playing';
+        break;
+      default:
+        this.status = 'idle';
+        break;
+    }
 
-  /**
-   * Starts the game.
-   */
-  start() {}
+    return this.status;
+  }
 
-  /**
-   * Resets the game.
-   */
-  restart() {}
+  start() {
+    this.board = JSON.parse(JSON.stringify(this.initialBoard));
 
-  // Add your own methods here
+    this.isGameActive = true;
+    this.placeNewCell();
+    this.placeNewCell();
+  }
+
+  restart() {
+    this.board = JSON.parse(JSON.stringify(this.initialBoard));
+    this.boardScore = 0;
+
+    this.currentStatus = 'idle';
+    this.isGameActive = false;
+    this.isGameWon = false;
+    this.isGameLose = false;
+  }
+
+  placeNewCell() {
+    let rRow, rColumn;
+
+    do {
+      rRow = Math.floor(Math.random() * 4);
+      rColumn = Math.floor(Math.random() * 4);
+    } while (this.board[rRow][rColumn] !== 0);
+
+    this.board[rRow][rColumn] = this.createNewCell();
+  }
+
+  createNewCell() {
+    const randomValue = Math.random();
+
+    return randomValue < 0.1 ? 4 : 2;
+  }
+
+  move(direction) {
+    if (!this.isGameActive) {
+      return;
+    }
+
+    const numCols = this.board[0].length;
+    let currentTable = JSON.parse(JSON.stringify(this.board));
+    let addScore = 0;
+
+    const transpose = (table) => {
+      return table[0].map((_, colIndex) => table.map((row) => row[colIndex]));
+    };
+
+    const reverseRow = (table) => {
+      return table.map((row) => row.slice().reverse());
+    };
+
+    const moveTable = (table) => {
+      const newTable = table.map((row) => {
+        let newRow = row.filter((num) => num !== 0);
+        const zeroToAdd = numCols - newRow.length;
+
+        newRow = [...Array(zeroToAdd).fill(0), ...newRow];
+
+        for (let i = newRow.length; i >= 0; i--) {
+          if (newRow[i - 1] === newRow[i]) {
+            newRow[i - 1] *= 2;
+            newRow[i] = 0;
+            addScore += newRow[i - 1];
+            i--;
+          }
+        }
+
+        newRow = newRow.filter((num) => num !== 0);
+
+        const zerosToAddEnd = numCols - newRow.length;
+
+        newRow = [...Array(zerosToAddEnd).fill(0), ...newRow];
+
+        return newRow;
+      });
+
+      return newTable;
+    };
+
+    const isGameOver = () => {
+      const Gamefield = 4;
+
+      for (let i = 0; i < Gamefield; i++) {
+        for (let j = 0; j < Gamefield; j++) {
+          if (this.board[i][j] === 0) {
+            return false;
+          }
+        }
+      }
+
+      for (let i = 0; i < Gamefield; i++) {
+        for (let j = 0; j < Gamefield; j++) {
+          if (j < Gamefield - 1 && this.board[i][j] === this.board[i][j + 1]) {
+            return false;
+          }
+
+          if (i < Gamefield - 1 && this.board[i][j] === this.board[i + 1][j]) {
+            return false;
+          }
+        }
+      }
+
+      this.isGameActive = false;
+      this.isGameLose = true;
+
+      return true;
+    };
+
+    const makeMove = (moveToSide) => {
+      if (JSON.stringify(moveToSide) === JSON.stringify(currentTable)) {
+        return;
+      }
+
+      this.board = moveToSide;
+      currentTable = moveToSide;
+
+      if (this.board.flat().includes(2048)) {
+        this.isGameActive = false;
+        this.isGameWon = true;
+
+        this.getStatus();
+
+        return;
+      }
+
+      this.placeNewCell();
+
+      this.isAbleMove = !isGameOver();
+    };
+
+    switch (direction) {
+      case 'up':
+        const moveUp = transpose(
+          reverseRow(moveTable(reverseRow(transpose(currentTable)))),
+        );
+
+        makeMove(moveUp);
+
+        this.boardScore += addScore;
+        break;
+      case 'down':
+        const moveDown = transpose(moveTable(transpose(currentTable)));
+
+        makeMove(moveDown);
+
+        this.boardScore += addScore;
+        break;
+      case 'right':
+        const moveRight = moveTable(currentTable);
+
+        makeMove(moveRight);
+
+        this.boardScore += addScore;
+        break;
+      case 'left':
+        const moveLeft = reverseRow(moveTable(reverseRow(currentTable)));
+
+        makeMove(moveLeft);
+
+        this.boardScore += addScore;
+        break;
+    }
+  }
 }
 
-module.exports = Game;
+export default Game;
